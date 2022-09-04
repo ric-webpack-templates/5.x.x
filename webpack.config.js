d8232339a358a85c29cddfe2c126ce99975c26aa
Richard PAYET <rick.info.rsoc@gmail.com>
2022-09-04 15:02:58 +0200

    Moyen de pouvoir signfier le mode (dev ou prod) depuis package.json, via vars d'env. pour WebPack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,89 +1,65 @@
-const oPathTool = require("path");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+class MyConfiguration {
 
-
-const oConfig = {
-
-  mode: "development"  
+  constructor(webPackEnvironmentVars) {
+    this.webPackEnvironmentVars = webPackEnvironmentVars;
+  }
   
-  , entry: {
-    //--- Ensemble des fichiers à "bundler" ----
+  getCommonConfig() {
+    const oMyCommonConfig = require("./webpack.config.common.js");
+    return oMyCommonConfig;
 
-     // 1er bundle :  ici 1 seul fichier .ts + bien sûr indirectement ses dépendances (.ts & .js) éventuelles.
-     //               Le nom du bundle une fois buildé, sera (cf. output.filename ci-dessous) :  
-     //                 allJS.bundle.js et il sera mis dans le sous-dossier js/
-     //                 du répertoire de build.    
-     // Ce allJS.bundle.js sera bien sûr à inclure dans index.html, via <script src="...">.
-     // NECESSITE bien sûr un TS loader(parse/transpile des TS pour les intégrer au bundle) 
-        "js/allJS": ["./_src/app/index.ts"], 
+  }// Fin getCommonConfig()
 
+  //======================================================================
 
-     // 2eme bundle :  plusieurs régles .css, mergées en 1 bundle à partir de fichiers .css.
-     //                  (remarque: lorsque doublon de règle css, la dernière apparue fait foi).
-     //                Le nom du bundle une fois buildé, sera (cf. output.filename ci-dessous) :  
-     //                 allCSS.bundle.js et il sera mis dans le sous-dossier css/
-     //                 du répertoire de build.    
-     // Ce allCSS.bundle.js sera bien sûr à inclure dans index.html, via <script src="...">.
-     //  Remarque : bien que ce bundle à inclure contiennent du code javascript,
-     //             les règles .css évoquées, feront bien leur effet sur l'html !
-     // NECESSITE bien sûr un css loader(lecture des css pour les intégrer au dit bundle) 
-     //          + un style loader (rendre ces règles css exploitables par du html)
-        "css/allCSS": [
-          "./_src/app/css/styles1.css", 
-          "./_src/app/css/styles2.css", 
-        ],
+  /*** JE PROCEDE AINSI , parce-qu'avec Webpack5, je ne suis pas parvenu à faire fonctionner
+    avec des fichiers de config. autres (un de dev et un de prod, exploitant chacun un autre contenant la common config.)
+    ceci en utilisant l'option --config de WebPack.
+    Du coup ici, je contourne en le laissant pointer vers son fichier de config. par défaut (webpack.config.js).
 
-  }
-  ,output: {
-      path: oPathTool.resolve(__dirname, `./_build`)        
-      ,filename: "[name].bundle.js" //[name]  = valeur de chacune des clefs de l'objet entry ci-dessus.
-                                    // Génèrera donc ces bundles :
-                                    //   ./_build/js/allJS.bundle.js
-                                    //   ./_build/css/allCSS.bundle.js //<<< Ok il suffira de l'inclure dans index.html via <script src="...">
+     Une histoire de souci avec "require" et le chemin de rech. des fichiers... 
+     Ce n'est pas si grave tant pis :) .
+  */
+  getConfig() {
+    const oCommonConfig = this.getCommonConfig();
+    const oModeConfigComplement = (this.isDevMode())? 
+        this.getDevModeConfigComplement() 
+      : this.getProdModeConfigComplement();
+
+    const oConfig = Object.assign(oCommonConfig, oModeConfigComplement);
+    return oConfig;
   }
 
+  isDevMode() { //IS_MODE_DEV var d'env. perso. 
+                //(que j'ai settée dans les vars d'env. de WebPack en ligne de commande de webpack, param --env).
+    return (this.webPackEnvironmentVars.IS_MODE_DEV==="true");
+  }  
 
-  //--------------------------------------
-
-  , module: {
-    
-    //-- Divers loader et, patterns des fichiers les concernant --
-    rules: [ 
-      //Pour gestion des fichiers .ts :
-      {
-        test: /\.ts$/  //Extension des fichiers à traiter (ne pas mettre entre guillemets !)
-        ,use: [ 
-          { loader: "ts-loader"  }  //Parse et rend utilisable le contenu des fichiers .ts
-        ]
-      },
-
-      //Pour gestion des fichiers .css
-      {
-        test: /\.css$/  //Extension des fichiers à traiter (ne pas mettre entre guillemets !)
-        ,use: [ 
-           { loader: "style-loader" }, //Gestion de l'inclusion des styles détectés.
-           { loader: "css-loader" } //Parse et rend utilisable le contenu des fichiers .css
-        ] ///<<<<<< ATTENTION : IL FAUT mettre "style-loader" avant "css-loader" sinon plantage !! <<<<<<<<
-      },
-      
-    ]
+  getDevModeConfigComplement() {
+    const retour = {
+      mode: "development"    //REM.: Si on commente cette ligne ce sera "production" par défaut, et donc le bundle de sortie sera illisible/minifié (sur une ligne).
+    };
+    return retour;   
   }
-  ,resolve: {
-    extensions: [ ".ts", ".js" ], // ".js" juste rajouté pour pouvoir traiter aussi en entrée des .js.
+  getProdModeConfigComplement() {
+    const retour = {
+      mode: "production"
+    };
+    return retour;   
+  }  
+}
 
-    alias:{ //MAPPAGES, pour imports de : modules avec chemin non-relatifs (cf. projets Typescript d'essais)
 
-    },
+module.exports = (webPackEnvironmentVars) => { //Lorsque module.exports renvoie une lambda, elle 
+                                               //peut recevoir en param. les vars d'env. WebPack
+                                               //(qui comprennent également celles perso. settées
+                                               //sur la ligne de commande WebPack via param. --env
+                                               //cf. package.json)
+  console.log(webPackEnvironmentVars);
+  //console.log(typeof(webPackEnvironmentVars.IS_MODE_DEV)); //string
 
-    plugins: [
-      new TsconfigPathsPlugin({ //Plugin permettant que la rubrique "paths:" du tsconfig.json, soit bien prise en compte et exploitable par le code .ts.
-        configFile: "./tsconfig.json"
-      }),
-    ]    
+  const oMyConfiguration = new MyConfiguration(webPackEnvironmentVars);
+  const oConfig = oMyConfiguration.getConfig(); 
 
-  }
-
+  return oConfig; //Et bien sûr il nous faut retourner la config. !
 };
-
-
-module.exports = oConfig;
